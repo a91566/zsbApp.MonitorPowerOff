b7a2e9fef5ecc15acb052db3d47b792afdd790a1
zhengShaobao <a91566@163.com>
2018-06-24 09:44:26 +0800

    举头望明月

---
+++ zsbApp.MonitorPowerOff/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace zsbApp.MonitorPowerOff
+{
+    class Program
+    {
+        private const uint WM_SYSCOMMAND = 0x112; //系统消息
+        private const int SC_MONITORPOWER = 0xF170; //关闭显示器的系统命令
+        private const int FLAG_MONITOR_POWEROFF = 2; //2为PowerOff, 1为省电状态，-1为开机
+        private static readonly IntPtr HWND_BROADCAST = new IntPtr(0xffff); //广播消息，所有顶级窗体都会接收
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
+
+
+        static void Main(string[] args)
+        {
+            System.Timers.Timer t = new System.Timers.Timer(50);
+            t.Enabled = true;
+            t.Elapsed+=(s, e) => System.Environment.Exit(0);
+            SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, FLAG_MONITOR_POWEROFF);
+        }
+    }
+}
